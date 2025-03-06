1d27707e86ccdf51adf0412687c1366f6c5d1b53
Vishvaraj18 <vishvaraj.dgkgr22@sinhgad.edu>
2025-03-06 16:07:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import CreateNote from "./CreateNote";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+import { useState } from "react"
+
 export default function App(){
+  const [notes,setNotes] = useState([])
+
+  function addNote(note){
+    setNotes(prevNotes =>{
+      return [...prevNotes,note]
+    })
+ 
+  }
+  function deleteNote(id){
+    setNotes(prevNotes => {
+    return prevNotes.filter((note,i)=>{
+        return i !== id;
+      })
+    }
+    )  
+    
+    
+
+  }
   return (
     <>
-    
+    <Header/>
+    <CreateNote onAdd = {addNote}/>
+   {notes.map((note,index) => {
+    return  <Note key = {index} id = {index} del={deleteNote}  note = {note}/>
+   })}
+   
+    <Footer/>
     </>
   )
 }
